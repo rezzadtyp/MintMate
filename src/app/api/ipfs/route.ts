af209265650f691d4530419cfb4899364c0425e9
rezzadtyp <rezzadityap@gmail.com>
2024-09-30 19:42:59 +0700

    fix: upload image to cloud

--- src/app/api/ipfs/route.ts
+++ src/app/api/ipfs/route.ts
@@ -2,8 +2,7 @@ import { NextResponse } from "next/server";
 import * as dotenv from "dotenv";
 import { ThirdwebStorage } from "@thirdweb-dev/storage";
 import { client } from "@/app/client";
-import fs from "fs/promises";
-import path from "path";
+import cloudinary from "cloudinary";
 
 dotenv.config();
 
@@ -11,6 +10,33 @@ const storage = new ThirdwebStorage({
   clientId: client.clientId,
   secretKey: client.secretKey,
 });
+
+cloudinary.v2.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET,
+});
+
+const uploadToCloudinary = (buffer: Buffer): Promise<string> => {
+  return new Promise((resolve, reject) => {
+    const uploadStream = cloudinary.v2.uploader.upload_stream(
+      {
+        folder: "mintmate",
+        allowed_formats: ["jpeg", "png", "jpg"],
+      },
+      (error, result) => {
+        if (error) {
+          reject(error);
+        } else if (result && result.secure_url) {
+          resolve(result.secure_url);
+        } else {
+          reject(new Error("Cloudinary upload failed: secure_url not found"));
+        }
+      }
+    );
+    uploadStream.end(buffer);
+  });
+};
 
 export async function POST(request: Request) {
   try {
@@ -36,16 +62,15 @@ export async function POST(request: Request) {
 
     // Step 1: Convert image file to path
     const buffer = Buffer.from(await imageFile.arrayBuffer());
-    const imagePath = path.join(
-      process.cwd(),
-      "public/assets/" + Date.now() + imageFile.name
-    );
+    const cloudinaryUrl = await uploadToCloudinary(buffer);
+    console.log(cloudinaryUrl);
 
-    await fs.writeFile(imagePath, buffer);
+    if (!cloudinaryUrl) {
+      throw new Error("Failed to upload image to Cloudinary");
+    }
 
-    const fileData = await fs.readFile(imagePath);
     // Step 2: Upload image to IPFS
-    const uri = await storage.upload(fileData);
+    const uri = await storage.upload(cloudinaryUrl);
 
     const NFTsMetadata = {
       name: name.toString(),
